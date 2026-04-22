6ac12cbf0700b816d4e3671311a3112825de1057
carlos.andres.pt@hotmail.com <carlos.andres.pt@hotmail.com>
2026-04-22 20:58:06 +0200

    commit 2

--- main.c
+++ main.c
@@ -5,15 +5,44 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
+     //int dice_roll = (rand() %6) +1 ;
+    int numbers[100];
 
-        int numbers[100];
+        int num1 = 0;
+        int num2 = 0;    
+        int num3 = 0;
+        int num4 = 0;
+        int num5 = 0;
+        int num6 = 0;
+  
+    for(int i = 0; i<100; i++){
+        numbers[i]=(rand()%6) +1;
+        } 
 
-        int dice_roll = (rand() %6) +1 ;
-        
-        for(int i = 0; i <100; i++ )
+    for(int i = 0; i <100; i++ ){
+       // printf("%d %d ", numbers[i], i);
+        if (numbers[i]==1) {
+            num1++;
+         }
+           if (numbers[i]==2) {
+            num2++;
+         }
+           if (numbers[i]==3) {
+            num3++;
+         }
+           if (numbers[i]==4) {
+            num4++;
+         }
+           if (numbers[i]==5) {
+            num5++;
+         }
+           if (numbers[i]==6) {
+            num6++;
+         }
+    }
 
+    printf(" %d %d %d %d %d %d",num1, num2, num3, num4, num5, num6);
     
-    // Your code goes here
 
     return 0;
 }
