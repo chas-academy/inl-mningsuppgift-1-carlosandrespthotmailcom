965bac11f572ec6c84f1000401ae50aea190e629
carlos.andres.pt@hotmail.com <carlos.andres.pt@hotmail.com>
2026-04-22 20:28:46 +0200

    Commit 1

--- main.c
+++ main.c
@@ -6,7 +6,13 @@ int main()
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
 
-   
+        int numbers[100];
+
+        int dice_roll = (rand() %6) +1 ;
+        
+        for(int i = 0; i <100; i++ )
+
+    
     // Your code goes here
 
     return 0;
